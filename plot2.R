bca8c7e1ebc936359f7f00665bc89c4f916c15a1
hairB <hairb96@gmail.com>
2014-10-12 16:48:02 -0400

    Project 1 files
    R code for generating plots + PNG files of the plots.

---
+++ plot2.R
@@ -0,0 +1,17 @@
+library(sqldf)
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+tempFile <- tempfile()
+download.file(fileURL, tempFile)
+unzip(tempFile)
+
+data <- read.csv.sql("household_power_consumption.txt", 
+                     sql="select * from file where Date in ('2/2/2007','1/2/2007')",
+                     header=TRUE, sep=";")
+
+data$DateTime <- strptime(paste(data$Date, data$Time,sep=" "), format="%d/%m/%Y %H:%M:%S")
+data$DayOfWeek <- strftime(data$DateTime,'%A')
+png("plot2.PNG")
+plot(data$DateTime, data$Global_active_power,
+     ylab="Global Active Power (kilowatts)", xlab="", type="l")
+dev.off()
+unlink(tempFile)
